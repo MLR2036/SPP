832b16061396a888f433e7eef3865c3165d00b96
RNaddaf <naddafr@yahoo.com>
2019-04-22 16:43:20 +0100

    routing bug fixes

--- shieldspremisespersonnel/src/app/app-routing.module.ts
+++ shieldspremisespersonnel/src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { AboutusComponent } from './components/aboutus/aboutus.component';
-import { ContactusComponent } from './components/contactus/contactus.component';
+import { DashboardComponent } from '../app/components/dashboard/dashboard.component';
+import { AboutusComponent } from '../app/components/aboutus/aboutus.component';
+import { ContactusComponent } from '../app/components/contactus/contactus.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/components/dashboard' },
-  { path: '/components/dashboard', component: DashboardComponent },
-  { path: '/components/aboutus', component: AboutusComponent },
-  { path: '/components/contactus', component: ContactusComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'components/dashboard' },
+  { path: 'components/dashboard', component: DashboardComponent },
+  { path: 'components/aboutus', component: AboutusComponent },
+  { path: 'components/contactus', component: ContactusComponent },
 
 ];
 
