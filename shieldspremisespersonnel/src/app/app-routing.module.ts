e4a9f47c0eff23c9a8e7e137c3bb374fd2a17612
marcus rodney <marcus.rodney@hotmail.co.uk>
2019-05-06 04:57:23 +0100

    added maintenance updated tar package dependencies

--- shieldspremisespersonnel/src/app/app-routing.module.ts
+++ shieldspremisespersonnel/src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from '../app/components/dashboard/dashboard.component';
 import { AboutusComponent } from '../app/components/aboutus/aboutus.component';
 import { ContactusComponent } from '../app/components/contactus/contactus.component';
+import { MaintenanceComponent } from '../app/maintenance/maintenance.component';
+
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'components/dashboard' },
   { path: 'components/dashboard', component: DashboardComponent },
   { path: 'components/aboutus', component: AboutusComponent },
   { path: 'components/contactus', component: ContactusComponent },
+  { path: 'maintenance', component: MaintenanceComponent },
 
 ];
 
---
+++ shieldspremisespersonnel/src/app/maintenance/maintenance.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+const newLocal = './maintenance.component.html';
+@Component({
+  selector: 'app-maintenance',
+  templateUrl: newLocal,
+  styleUrls: ['./maintenance.component.css']
+
+ 
+})
+export class MaintenanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
