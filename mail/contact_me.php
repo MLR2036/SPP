33c7b7f86876d3b60a69418afa61279a21169dab
marcus rodney <marcus.rodney@hotmail.co.uk>
2020-04-02 21:43:46 +0100

    added new sections, backgrong pictures ,modals

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -7,10 +7,10 @@ use PHPMailer\PHPMailer\PHPMailer;
 require '../vendor/phpmailer/phpmailer/src/PHPMailer.php';
 
 // Check for empty fields
-if (empty($_POST['name']) 
-    || empty($_POST['email']) 
-    || empty($_POST['phone'])     
-    || empty($_POST['message'])   
+if (empty($_POST['name'])
+    || empty($_POST['email'])
+    || empty($_POST['phone'])
+    || empty($_POST['message'])
     || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)
 ) {
     echo "No arguments Provided!";
